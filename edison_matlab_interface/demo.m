6a1207ef7b0169d2e6eb1a35c4a3a05d011a8939
ChienWenYang <41105062+ChienWenYang@users.noreply.github.com>
2022-05-21 15:47:52 +0800

    Delete edison_matlab_interface directory

--- edison_matlab_interface/demo.m
+++
@@ -1,7 +0,0 @@
-
-function demo
-I=im2double(imread('butterfly00285.jpg'));
-
-[ISeg ILabel modes regSize grad conf]=edison_wrapper(I,@RGB2Luv,'step',2,'SpatialBandWidth',5,'RangeBandWidth',3,'MinimumRegionArea',100);
-
-return;
--- edison_matlab_interface/nochange.m
+++
@@ -1,7 +0,0 @@
-function [ I2 ] = nochange( I )
-%NOCHANGE Summary of this function goes here
-%   Detailed explanation goes here
-I2=I;
-
-end
-
